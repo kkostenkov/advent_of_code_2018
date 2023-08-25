90af25c538494ab6e35bbd3c5fdb35e81dc64220
Bugord <kakareko.yurij@yandex.ru>
2023-08-25 17:18:09 +0200

    Add new testcases, fix testcase for recorded frequencies

--- AOC2018/AOC2018/Device.cs
+++ AOC2018/AOC2018/Device.cs
@@ -2,17 +2,22 @@ namespace AOC2018;
 
 public class Device
 {
+    private int[] visitedFrequencies;
+        
     public int Calibrate(string calibrationData)
     {
         var parser = new CalibrationDataParser();
         var numbers = parser.Parse(calibrationData);
+
+        visitedFrequencies = numbers;
+        
         var result = numbers.Sum();
 
         return result;
     }
 
-    public int[] GetRecordedFrequences()
+    public int GetFirstDuplicatedFrequency()
     {
-        throw new NotImplementedException();
+        return 0;
     }
 }
--- AOC2018/Tests/DeviceTest.cs
+++ AOC2018/Tests/DeviceTest.cs
@@ -16,12 +16,15 @@ public class Tests
     }
 
     [Test]
-    [TestCase("+1,\n+1,\n+1", new [] { 1, 1, 1 })]
-    public void Should_RecordVisitedFrequences_When_GivenInput(string calibrationData, int[] expectedResult)
+    [TestCase("+1,\n+1", 0)]
+    [TestCase("+3,\n+3,\n+4,\n-2,\n-4", 10)]
+    [TestCase("-6,\n+3,\n+8,\n+5,\n-6", 5)]
+    [TestCase("+7,\n+7,\n-2,\n-7,\n-4", 14)]
+    public void Should_RecordVisitedFrequences_When_GivenInput(string calibrationData, int expectedResult)
     {
         var device = new Device();
         device.Calibrate(calibrationData);
-        var recordedFrequences = device.GetRecordedFrequences();
+        var recordedFrequences = device.GetFirstDuplicatedFrequency();
         
         Assert.AreEqual(expectedResult, recordedFrequences);
     }
